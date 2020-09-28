f36967e72ab4815bb51d9a34a217d1b0d5a27198
Renshix <71999726+Renshix@users.noreply.github.com>
2020-09-27 23:05:21 -0500

    Add files via upload

---
+++ OROZCO_ World.s
@@ -0,0 +1,37 @@
+PROCESSOR 16F887
+#include <xc.inc>
+;CONFIG word1
+CONFIG FOSC = INTRC_NOCLKOUT
+CONFIG WDTE = OFF
+CONFIG PWRTE = ON
+CONFIG MCLRE = OFF
+CONFIG FOSC = INTRC_NOCLKOUT
+CONFIG WDTE = OFF
+CONFIG PWRTE = ON
+CONFIG MCLRE = OFF
+CONFIG CP = OFF
+
+;CONFIG word2
+CONFIG BOR4V=BOR40V
+CONFIG WRT = OFF
+PSECT udata
+ mask:
+    DS 1
+cont:
+    DS 1
+var3:
+    DS 1
+
+PSECT resetVec,class=CODE,delta=2
+resetVec:
+PAGESEL main
+goto main
+PSECT code
+ main:
+ bcf 0x03,6
+ bcf 0x03,5
+ movlw 0b10010010
+ ANDLW 0b01111111
+ movwf mask
+ goto main
+ END
